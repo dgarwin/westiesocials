7a9c22a8f502e004f81de239914e48c8dd000e64
David Garwin <davidgarwin@gmail.com>
2023-09-04 01:54:43 -0400

    Updates prompt and code. Needs to re-add recursive filtering. Needs to add searching by metropolitan area

--- main.py
+++ main.py
@@ -24,27 +24,6 @@ options.headless = True
 driver = webdriver.Chrome(options=options)
 
 
-# Define the structure of a Social
-class Social:
-    def __init__(self, raw_row: str, state: str, source_url: str):
-        columns = raw_row.split(',')
-        self.state = state
-        self.title = columns[0]
-        self.source_url = source_url
-        self.day_of_week = columns[1]
-
-    def to_string(self):
-        print(f"{self.state}, {self.title}, {self.day_of_week}, {self.source_url}")
-
-    def to_dict(self):
-        return {
-            "state": self.state,
-            "day_of_week": self.day_of_week,
-            "title": self.title,
-            "source_url": self.source_url
-        }
-
-
 # List of US states
 states = [
     "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
@@ -58,7 +37,11 @@ states = [
     "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
     "West Virginia", "Wisconsin", "Wyoming", "Washington DC"
 ]
-states = ['New York']
+states = ['Texas']
+
+known_dances = {
+    'New York': ['Flow Fridays']
+}
 
 
 def wait_for_element(by, value, timeout=10):
@@ -75,14 +58,14 @@ def get_page_html(url, recursive: bool = True) -> List[str]:
 
         # Parse page source with BeautifulSoup
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        ret = [soup.body.get_text(separator=' ', strip=True)]
+        ret = [soup.body.get_text(separator='---', strip=True)]
     except Exception as e:
         print(f"Error getting text for {url}: {e}")
         raise e
     if recursive:
         links = filter_domain_links_for_socials([a['href'] for a in soup.find_all('a', href=True)
                                                  if get_domain(a['href']) == domain])
-        links_formatted = '\n' + '\n\t'.join(links)
+        links_formatted = '\n\t' + '\n\t'.join(links)
         print(f"Found additional sub-pages to search: {links_formatted}")
         for link in links:
             ret.extend(get_page_html(link, False))
@@ -93,10 +76,13 @@ def filter_domain_links_for_socials(links: List[str]):
     system_msg = """
     You are a master of webpage navigation. 
     When given a list of links, you will return the links matching the prompt provided.
-    You will only return a comma separated list of links. 
+    After explaining your reasoning write on a new line STARTING_OUTPUT and output links starting on \
+    the next line.
     EX: If you are given 
     http://a.com,http://b.com,http://c.com
     and you are asked to return links to everywhere but c.com, you will return
+    {Reasoning}...
+    STARTING_OUTPUT
     http://a.com,http://b.com
     """
     user_msg = f"""
@@ -112,29 +98,51 @@ def filter_domain_links_for_socials(links: List[str]):
     raw = openai.ChatCompletion.create(model="gpt-4",
                                        messages=[{"role": "system", "content": system_msg},
                                                  {"role": "user", "content": user_msg}])
-    response: str = raw["choices"][0]["message"]["content"]
+    response: str = raw["choices"][0]["message"]["content"].split('STARTING_OUTPUT\n')[1]
     return response.split(',')
 
 
 def get_socials_from_page(webpage, state, source_url):
+    if state in known_dances:
+        state_dances = known_dances[state]
+    else:
+        state_dances = []
     # Define the system message
     system_msg = """
-    You are a CSV generator. You do nothing but generate CSVs. All of your ouptuts are 
-    a comma separated list of strings to represent rows and newlines. 
-    If you output any other text, I will be sad and you do not want me sad.
-    You do not print header rows, only data rows.
+    You are great at parsing website output from beautiful soup. 
+    The separator used to concatenate text is "---".
+    You are great at generating CSVs rows; you do not output headings.
+    Based on the prompt about the website, you will generate a CSV output.
+    You explain your reasoning for rows generated based on quotes from the website text.
+    After explaining your reasoning write on a new line STARTING_OUTPUT and output rows starting on the next line.
+    If are no rows to output, write NO_OUTPUT instead of STARTING_OUTPUT. Do not write both at the same time.
     """
 
     # Define the user message
     user_msg = f"""
-    Below is the output of a website about West Coast Swing. 
-    Parse the site and output a CSV with all the known West Coast Swing socials. 
-    If there are no socials, return no text. If you find any socials, output rows of the form
-    Title, DayOfWeek
+    Below is the output of a website about West Coast Swing that may contain information about social dances.
+    We want all the social dances we can find with an organizing group or name of event, location, and day of the week.
+    If we do not know either of those pieces of information, do not add a row.
+    Information about the lesson (EX Advanced, Beginner, etc) should not be included.
+    The first column is the organizing group or name of the event (there's generally only one or the other), \
+    the second is the name of the location, the third is the day of the week. 
+    Only output the day of the week (EX Monday), not which days of the month (EX: Every 3rd Monday).
+    If there is both an organizing group and name of event, only output a single row using the organizing group.
+    If the name of the location is not present, leave a ?
+    The known organizers in this area are: {','.join(state_dances)}. \
+    There are likely to be additional organizers we don't know about.
+    If there is no organizer, treat the name of the event as the organizer.
+    If there is no day of the week, but there is a date (EX October 7) put the date in the form MM/DD/YY instead.\
+    If there is no year EXPLICITLY stated, output the date in terms of MM/DD.
+    If two events are on the same day of the week and have similar names, assume they are referring to the same event.
+    In the world of west coast swing there are weekend long events (generally Thurs/Fri to Sunday). \
+    Do not include these or any social dancing that occurs as a part of this event. 
     Samples (DO NOT USE THESE IN OUTPUT):
-    "Every first Saturday, join the Gotham Swing Club for their monthly dance." -> Gotham Swing, Saturday
+    "Every first Saturday, join the Gotham Swing Club at Best Dance Studio, for their monthly dance." ->\
+     Gotham Swing, Best Dance Studio, Saturday
     "Tuesday Night West Coast Swing The Electric Belle in Stovehouse @ 6:30 PM | Social Dancing @ 7:15 PM"\
-     -> Electric Belle in StoveHouse, Tuesday 
+     -> ?, Electric Belle, Tuesday 
+    Negative Examples: 
     Page:
     {webpage}
     """
@@ -143,9 +151,13 @@ def get_socials_from_page(webpage, state, source_url):
     raw = openai.ChatCompletion.create(model="gpt-4",
                                        messages=[{"role": "system", "content": system_msg},
                                                  {"role": "user", "content": user_msg}])
-    response = raw["choices"][0]["message"]["content"]
-    return [Social(raw_row, state, source_url)
-            for raw_row in response.splitlines()]
+    print(f'get_socials_from_page\n{raw["choices"][0]["message"]["content"]}')
+    res = raw["choices"][0]["message"]["content"].split('STARTING_OUTPUT\n')
+    if len(res) != 2:
+        return []
+    else:
+        response = raw["choices"][0]["message"]["content"].split('STARTING_OUTPUT\n')[1]
+        return [f"{state},{raw_row},{source_url}" for raw_row in response.splitlines()]
 
 
 def filter_blocked_domains(urls, blocked_domains):
@@ -169,6 +181,35 @@ def get_domain(url):
         return match.group(1)
     else:
         return None
+
+
+def remove_duplicates(csv_string):
+    system_msg = """
+    You are great at removing duplicate rows from CSVs. People give you CSVs with some prompt, and you answer the \
+    prompt, returning a CSV at the end with removed duplicate rows. Every time you remove a row, you explain your \
+    logic, pointing at the row that it is a duplicate of.
+    """
+    user_msg = f"""
+    Below is a CSV of west coast swing socials. Columns are: State, Organizer, Location, Day of Week, Source URL. \
+    We consider something a duplicate if an event is run by the same organizer on the same day. 
+    Sometimes, we may have organizer names typed differently (EX: MDJ WCS, MDJ, Millenium Dance Jam, Lori Brizzi's \
+    MDJ) are all the same. In our output row, we want to use the shortest organizer name.
+    If we don't know information, we may put a ? or a MANUAL_INTERVENTION_NEEDED token. If rows are \
+    duplicated, we want to make sure to combine data from all rows to maximize the amount of data we have.
+    After explaining your reasoning write on a new line STARTING_OUTPUT and output the filtered CSV starting on \
+    the next line.
+    CSV:
+    {csv_string}
+    """
+    raw = openai.ChatCompletion.create(model="gpt-4",
+                                       messages=[{"role": "system", "content": system_msg},
+                                                 {"role": "user", "content": user_msg}])
+    print(f'get_socials_from_page\n{raw["choices"][0]["message"]["content"]}')
+    res = raw["choices"][0]["message"]["content"].split('STARTING_OUTPUT\n')
+    if len(res) == 1:
+        return []
+    else:
+        return raw["choices"][0]["message"]["content"].split('STARTING_OUTPUT\n')[1]
 
 
 def process_state(state):
@@ -196,16 +237,24 @@ def process_state(state):
             print(f"Skipping Website {website}")
             continue
         else:
+            print(f'Visiting {website}')
             visited_domains.add(domain)
         try:
-            contents = get_page_html(website)
-            for html_content in contents:
-                socials_to_add = get_socials_from_page(html_content, state, website)
-                state_socials.extend(socials_to_add)
+            contents = get_page_html(website, False)
         except Exception as e:
             print(f"Error processing link {website} for state {state}: {e}")
-    df = pd.DataFrame([social.to_dict() for social in state_socials])
-    df.to_csv(file_name, index=False)
+            continue
+
+        for html_content in contents:
+            try:
+                socials_to_add = get_socials_from_page(html_content, state, website)
+                if len(socials_to_add) > 0:
+                    state_socials.extend(socials_to_add)
+            except Exception as e:
+                print(f"Error processing contents {contents} for state {state}: {e}")
+    output = remove_duplicates('\n'.join(state_socials))
+    with open(file_name, 'w') as f:
+        f.write(output)
 
 
 def main():
